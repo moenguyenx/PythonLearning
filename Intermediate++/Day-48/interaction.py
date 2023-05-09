21dd8249c87a5481e10609a7ba1b9a35042d31ca
moenguyenx <qminh1908@gmail.com>
2023-05-09 14:11:47 +0700

    fifth commit

--- Intermediate++/Day-48/interaction.py
+++ Intermediate++/Day-48/interaction.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_drive_path = "D:\Personal Projects\ChromeDriver\chromedriver.exe"
 
 driver = webdriver.Chrome(service=Service(chrome_drive_path))
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(article_count.text)
+driver.get("https://web.archive.org/web/20220120120408/https://secure-retreat-92358.herokuapp.com/")
+# article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+
+first_name = driver.find_element(By.NAME, "fName")
+first_name.send_keys("dmdm")
+
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("ahaha")
+
+email = driver.find_element(By.NAME, "email")
+email.send_keys("ahihi@ohiihi.com")
+
+sign_up = driver.find_element(By.CSS_SELECTOR, "Button")
+sign_up.click()
+
+driver.quit()
