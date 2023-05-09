49ed681544a1f78204d36834edf4e4a175e0ded6
moenguyenx <qminh1908@gmail.com>
2023-05-09 09:29:10 +0700

    second commit

--- Intermediate++/Day-48/main.py
+++ Intermediate++/Day-48/main.py
@@ -9,3 +9,5 @@ driver = webdriver.Chrome(service=Service(chrome_driver_path))
 driver.get("https://www.python.org/")
 price = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
 print(price.text)
+
+print(price.id)
